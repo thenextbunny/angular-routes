346a6760b09d2ea06fd39dba1f38282a1ae94e12
Vitor <vitorcoelhosilvat@gmail.com>
2023-11-03 08:57:54 -0300

    Adição de arquivos da prática de roteamento

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+const routes: Routes = [
+  {
+    path: 'modulo1',
+    loadChildren: () =>
+      import('./modulo1/module1.module').then((m) => m.Modulo1Module),
+  },
+];
+
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule, RouterModule.forRoot(routes)],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
